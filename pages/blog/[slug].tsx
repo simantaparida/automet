e3f2a36f2e22f1993d3d76f228feae1efeb7cea4
simantaparida <simantaparidaux@gmail.com>
2025-11-07 06:19:59 +0530

    fix: Re-add Head import - it's actually being used

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -6,6 +6,7 @@
 
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 import Link from 'next/link';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
